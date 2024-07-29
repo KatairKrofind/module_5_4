a82dd7b47e74b0a7526e7b6f478c001b0b092d8f
KatairKrofind <bayandueva00@gmail.com>
2024-07-30 01:46:55 +0800

    Update module_5_4.py

--- module_5_4.py
+++ module_5_4.py
@@ -1,7 +1,7 @@
 class Building:
     total = 0
     def __init__(self):
-        for i in range(1, 41):
-            Building.total += 1
-            print(Building.total)
-h = Building()
+        Building.total += 1
+for i in range(40):
+    h = Building()
+    print(h)
